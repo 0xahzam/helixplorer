af775d963c27992139de5c73564d278a3b7ebca2
Ahzam Khan <ahzam.main@gmail.com>
2023-04-01 19:52:37 +0530

    meta tags

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,62 @@
 import "@/styles/globals.css";
 import { ChakraProvider } from "@chakra-ui/react";
-
+import Head from "next/head";
 export default function App({ Component, pageProps }) {
   return (
-    <ChakraProvider>
-      <Component {...pageProps} />
-    </ChakraProvider>
+    <>
+      <Head>
+        <title>Helixplorer</title>
+        <meta
+          name="viewport"
+          key="main"
+          content="width=device-width, initial-scale=1.0"
+        />
+
+        <meta name="title" content="Helixplorer" />
+        <meta
+          name="description"
+          content="A minimal beautiful wiki to visualize proteins in 3d and briefly read about them."
+        />
+
+        <meta property="og:type" content="website" key="og-type" />
+        <meta
+          property="og:url"
+          content="https://helixplorer.vercel.app/"
+          key="og-url"
+        />
+        <meta property="og:title" content="Helixplorer" key="og-title" />
+        <meta
+          property="og:description"
+          content="A minimal beautiful wiki to visualize proteins in 3d and briefly read about them."
+          key="og-desc"
+        />
+        <meta
+          property="og:image"
+          content="https://i.ibb.co/FKW2D82/Frame-39.png"
+          key="og-image"
+        />
+
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta
+          property="twitter:url"
+          content="https://helixplorer.vercel.app/"
+          key="twt-url"
+        />
+        <meta property="twitter:title" content="Helixplorer" key="twt-title" />
+        <meta
+          property="twitter:description"
+          content="A minimal beautiful wiki to visualize proteins in 3d and briefly read about them."
+          key="twt-desc"
+        />
+        <meta
+          property="twitter:image"
+          content="https://i.ibb.co/FKW2D82/Frame-39.png"
+          key="twt-img"
+        />
+      </Head>
+      <ChakraProvider>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </>
   );
 }
